d7dcf33f0f5a4078ac442a27fc3fda43ecfd0980
dmaser <git@gnarly.info>
2021-04-22 22:20:17 -0700

    Use only env values for billingBucket and notificationEmail

--- lib/billing-notifier-stack.ts
+++ lib/billing-notifier-stack.ts
@@ -4,6 +4,10 @@ import s3 = require('@aws-cdk/aws-s3');
 import { Bucket } from '@aws-cdk/aws-s3';
 import { Topic } from '@aws-cdk/aws-sns';
 import * as subscriptions from '@aws-cdk/aws-sns-subscriptions';
+// import * as events from '@aws-cdk/aws-lambda-event-sources';
+
+const billingBucketName = process.env.BILLING_BUCKET;
+const notificationEmail = process.env.NOTIFICATION_EMAIL;
 
 export class BillingNotifierStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -14,7 +18,10 @@ export class BillingNotifierStack extends cdk.Stack {
       displayName: 'Billing Notifier'
     });
 
-    const billingBucket = Bucket.fromBucketName(this, 'BillingBucketByName', 'dmaser-billing');
+    if (!billingBucketName || !notificationEmail) {
+      throw new Error('Both BILLING_BUCKET and NOTIFICATION_EMAIL env variables must be defined');
+    }
+    const billingBucket = Bucket.fromBucketName(this, 'BillingBucketByName', billingBucketName);
     const bucket = new s3.Bucket(this, 'billing-notifier');
 
     const bnLambda = new lambda.Function(this, 'BillingNotifierLambdaHandler', {
@@ -27,13 +34,26 @@ export class BillingNotifierStack extends cdk.Stack {
         TOPIC_ARN: snsTopic.topicArn
       },
     });
- 
+
     billingBucket.grantRead(bnLambda);
     // billingBucket.grantPut(bnLambda, 'diffs/*');
-    
+
     bucket.grantReadWrite(bnLambda);
     snsTopic.grantPublish(bnLambda);
-    snsTopic.addSubscription(new subscriptions.EmailSubscription('aws@davemaser.com'));
+    snsTopic.addSubscription(new subscriptions.EmailSubscription(notificationEmail));
 
+    // https://github.com/aws/aws-cdk/issues/2004
+    // bnLambda.addEventSource(new events.S3EventSource(
+    //   billingBucket,
+    //   {
+    //     events: [
+    //       s3.EventType.OBJECT_CREATED
+    //     ]
+    //   }
+    // ));
+
+    // aws s3api put-bucket-notification-configuration
+    // replaces current config
+    
   }
 }
